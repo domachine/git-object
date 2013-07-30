96f9153375410cc45c8f661b32f525af1aa8b0e3
Dominik Burgdörfer <dominik.burgdoerfer@mikanda.de>
2013-07-30 09:01:52 +0200

    fixed tests

--- test/git-object.js
+++ test/git-object.js
@@ -16,7 +16,7 @@ var fs = require('fs')
  * Tests.
  */
 
-describe('fromPath', function(){
+describe('fromPath()', function(){
   it('should create a blob', function(done){
     fromPath('.', 'master', 'index.js', function(err, object){
       if (err) return done(err);
@@ -25,34 +25,20 @@ describe('fromPath', function(){
       sink.on('data', function(d){
         d.toString().should.equal([
           '',
-          '/**',
-          ' * Module dependencies.',
-          ' */',
-          '',
-          "var template = require('./lib/template');",
-          '',
-          '/**',
-          ' * Module exports.',
-          ' */',
-          '',
-          'exports.template = template;',
-          ''
+          "module.exports = require('./lib/git-object');"
         ].join('\n'));
         done();
       });
     });
   });
   it('should create a tree', function(done){
-    fromPath('test/node-odt.git', 'master', 'lib', function(err, object){
+    fromPath('.', 'master', 'lib', function(err, object){
       if (err) return done(err);
       assert.ok(object instanceof fromPath.Tree);
       object.contents(function(err, contents){
         if (err) return done(err);
         contents.should.eql([
-          'handler/',
-          'odt-content.js',
-          'template.js',
-          'util.js'
+          'git-object.js'
         ]);
         done();
       });
